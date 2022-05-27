98019e796de4ddfbd17d692cb9a80cc1a66eb798
Fer Torres <fertorresmx@gmail.com>
2022-05-27 01:35:43 -0500

    adding post migration+model+seeder

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->text(40),
+            'slug' => STR::of($this->faker->text(10))->slug('-'),
+            'content' => $this->faker->paragraphs(10, true)
+        ];
+    }
+}
